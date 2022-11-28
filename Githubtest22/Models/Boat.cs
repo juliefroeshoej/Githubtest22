cb75d172ea0eb9a19cfcac82b8b3720e40bbea87
Sarah <sarahenya2000@hotmail.dk>
2022-11-28 10:03:01 +0100

    Added Boat in model folder

---
+++ Githubtest22/Models/Boat.cs
@@ -0,0 +1,9 @@
+﻿namespace Githubtest22.Models
+{
+    public class Boat
+    {
+        public int RegId { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
+    }
+}
